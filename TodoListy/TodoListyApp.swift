176ed589531315443520eff88f9ae4243c02402b
Aman Saini <sainiaman1090@gmail.com>
2025-08-29 16:42:45 +0530

    Initial Commit

---
+++ TodoListy/TodoListyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListyApp.swift
+//  TodoListy
+//
+//  Created by Aman on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListyTests/TodoListyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListyTests.swift
+//  TodoListyTests
+//
+//  Created by Aman on 29/08/25.
+//
+
+import Testing
+@testable import TodoListy
+
+struct TodoListyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
